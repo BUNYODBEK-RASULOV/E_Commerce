29ab0bc3fe2953b36593de456c2eb6f4fa926abf
lord_dev <ll>
2021-12-09 12:38:40 +0500

    "CategoryService and front classes are  created"

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -2,8 +2,10 @@ package repository;
 
 import models.Category;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface CategoryRepository extends BaseRepository<Category, String, List<Category>> {
 
+       List<Category> categoryList = new ArrayList<>();
 }
